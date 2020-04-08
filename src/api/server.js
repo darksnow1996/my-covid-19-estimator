2cd0b545d228914d05072c7cb8aa8b2a7a4a187b
oluyemodamola@gmail.com <oluyemodamola@gmail.com>
2020-04-08 10:59:32 -0700

    deployment changes 1

--- src/api/server.js
+++ src/api/server.js
@@ -53,6 +53,7 @@ app.use(function (err, req, res, next) {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port,function(){
+    console.log('Listening on port ', port);
+});
 
-console.log('Listening on port ', port);
